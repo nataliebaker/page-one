95404107eb9327df1162fffc4ec2db397a0aa1b1
Natalie Baker <natalie@Nathans-MacBook-Pro.local>
2013-04-13 14:11:33 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pageone::Application.config.secret_token = '8147750d02606b84865dd1a6bd80a957e47507d17341e9ac41ef2a6d340f3c078dee8c90dee0996f83e328c69f727e2c0ee44c548d028905b7c116dd98f2cb9d'
